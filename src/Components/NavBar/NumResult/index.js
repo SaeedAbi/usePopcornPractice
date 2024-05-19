cf2d390fbaf36fc69655d2a4359bc207e9067977
SaeedAbi <abdary.saeed@gmail.com>
2024-05-19 16:01:54 +0330

    :zap: add Optional chaining to NumResult

--- src/Components/NavBar/NumResult/index.js
+++ src/Components/NavBar/NumResult/index.js
@@ -1,6 +1,6 @@
 const NumResult=({movies})=> {
     return <p className="num-results">
-        Found <strong>{movies.length}</strong> results
+        Found <strong>{movies?.length}</strong> results
     </p>
 }
 export default NumResult
