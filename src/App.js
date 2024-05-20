dfd4a85ffcbc049682fd1c756e94f2629ccafac2
SaeedAbi <abdary.saeed@gmail.com>
2024-05-20 14:57:33 +0330

    :zap: add localStorage for watched list

--- src/App.js
+++ src/App.js
@@ -18,10 +18,13 @@ export const key='1068d90a'
 export default function App() {
     const [query,setQuery]=useState('')
     const [movies, setMovies] = useState([]);
-    const [watched, setWatched] = useState([]);
     const [isLoading,setIsLoading]=useState(false )
     const [error,setError]=useState('')
     const [selectedId,setSelectedId]=useState(null)
+    const [watched, setWatched] = useState(function(){
+            const storedValue = localStorage.getItem("watched");
+            return JSON.parse(storedValue);
+        });
 
 function handleSelectMovie (id){
 setSelectedId(selectedId=> id===selectedId? null:id)
@@ -32,11 +35,17 @@ setSelectedId(null)
 
 const handleAddWatched=(movie)=>{
         setWatched(watched=>[...watched,movie])
+
+    // localStorage.setItem('watched',JSON.stringify([...watched,movie]))
 }
 
 function handleDeleteWatched(id){
         setWatched(watched=>watched.filter(movie=>movie.imdbID !== id))
 }
+
+    useEffect(() => {
+        localStorage.setItem('watched',JSON.stringify(watched))
+    }, [watched]);
 
     useEffect(() => {
         const controller= new AbortController()
